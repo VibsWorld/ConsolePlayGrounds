de30c563a8a3c5e9b6a7f5eaf0f21ab9986db13c
Vaibhav Agarwal <vaibhav@parcelhero.com>
2025-03-16 16:47:46 +0530

    fix: rabbitmq connection errors and replace testcontainers.rabbitmq to testcontainers

--- Masstransits.GettingStarted/Program.cs
+++ Masstransits.GettingStarted/Program.cs
@@ -1,12 +1,15 @@
+using System.Reflection;
 using System.Threading.Tasks;
 using Docker.DotNet.Models;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Networks;
 using MassTransit;
 using Masstransits.Setup;
 using Masstransits.Setup.Consumers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Testcontainers.RabbitMq;
 
 //https://github.com/MassTransit/Sample-GettingStarted/tree/master#install-rabbitmq
 namespace Masstransits
@@ -16,24 +19,45 @@ namespace Masstransits
         private const string VirtualHost = "ParcelVision.Retail";
         private const string Username = "pvRetailDev";
         private const string Password = "pvRetailDev";
+
+        private const ushort RabbitMqHostPort = 5672;
+        private const ushort RabbitMqHttpManagementPort = 15672;
         private static ushort ContainerPort;
-        private static RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder()
-            .WithImage("rabbitmq:3-management")
-            .WithHostname(VirtualHost)
-            .WithUsername(Username)
-            .WithPassword(Password)
-            .WithPortBinding(RabbitMqBuilder.RabbitMqPort, false)
-            //.WithExposedPort(RabbitMqBuilder.RabbitMqPort)
-            .Build();
+        private static INetwork dockerNetwork = new NetworkBuilder().Build();
+        private static IContainer containerRabbitMq;
+
+            
 
         public static async Task Main(string[] args)
         {
-            await _rabbitMqContainer.StartAsync();
-            ContainerPort = _rabbitMqContainer.GetMappedPublicPort(RabbitMqBuilder.RabbitMqPort);
+            InitializeRabbitmqContainerOptions();
+            await containerRabbitMq.StartAsync();
+            ContainerPort = containerRabbitMq.GetMappedPublicPort(RabbitMqHostPort);
             var build = CreateHostBuilder(args).Build();
 
             await build.RunAsync();
         }
+
+        private static void InitializeRabbitmqContainerOptions() =>
+        containerRabbitMq = new ContainerBuilder()
+            .WithNetwork(dockerNetwork)
+            .WithImage("rabbitmq:3-management")
+            .WithEnvironment("RABBITMQ_DEFAULT_USER", Username)
+            .WithEnvironment("RABBITMQ_DEFAULT_PASS", Password)
+            .WithEnvironment("RABBITMQ_DEFAULT_VHOST", VirtualHost)
+            .WithPortBinding(RabbitMqHostPort, RabbitMqHostPort)
+            .WithPortBinding(RabbitMqHttpManagementPort, RabbitMqHttpManagementPort)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(RabbitMqHostPort))
+            .WithWaitStrategy(
+                Wait.ForUnixContainer().UntilPortIsAvailable(RabbitMqHttpManagementPort)
+            )
+            .WithWaitStrategy(
+                Wait.ForUnixContainer()
+                    .UntilHttpRequestIsSucceeded(request =>
+                        request.ForPort(RabbitMqHttpManagementPort).ForPath("/")
+                    )
+            )
+            .Build();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -42,8 +66,9 @@ namespace Masstransits
                     {
                         services.AddMassTransit(x =>
                         {
-                            // elided...
-                            x.AddConsumer<HelloWorldContractConsumer>();
+                            
+                            //x.AddConsumer<HelloWorldContractConsumer>();
+                            x.AddConsumers(Assembly.GetExecutingAssembly());
                             x.UsingRabbitMq(
                                 (context, cfg) =>
                                 {
