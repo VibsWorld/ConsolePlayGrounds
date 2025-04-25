d66169357b4ea74a0bad946d7e209d66e639f4d3
Vaibhav Agarwal <vaibhav@parcelhero.com>
2025-04-25 21:00:17 +0530

    serilog console sample

---
+++ Serilogs.ConsoleSample/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Serilogs.ConsoleSample;
+
+using Serilogs.Main;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var logger = SerilogHelper.GetLogger();
+        logger.Information("First Message");
+    }
+}
