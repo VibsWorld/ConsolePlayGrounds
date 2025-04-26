7f6c16e64ed662e6c513d2f943b739d777a9661a
Vaibhav Agarwal <vaibhav@parcelhero.com>
2025-04-26 22:57:59 +0530

    add authentication with username and password

--- Email.FluentEmails.Console/Program.cs
+++ Email.FluentEmails.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using FluentEmail.Core;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text.Json;
+using FluentEmail.Core;
 using Microsoft.Extensions.DependencyInjection;
 
 //https://github.com/rnwood/smtp4dev/blob/master/docker-compose.yml
@@ -15,18 +18,35 @@ namespace Email.FluentEmails.ConsoleTests
             var defaultFromEmail = "test@test.com";
             var host = "localhost";
             var port = 25;
-            //var userName = "test";
-            //var password = "test";
-            services.AddFluentEmail(defaultFromEmail).AddSmtpSender(host, port);
+            var userName = "test";
+            var password = "test";
+            //services.AddFluentEmail(defaultFromEmail).AddSmtpSender(host, port, userName, password);
+            var smtpClient = new SmtpClient(host, port)
+            {
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(userName, password),
+            };
+            services.AddFluentEmail(defaultFromEmail).AddSmtpSender(smtpClient);
             var provider = services.BuildServiceProvider();
 
+            //Parallel.For(
+            //    1,
+            //    1,
+            //    (i) =>
+            //    {
             var _fluentEmail = provider.GetService<IFluentEmail>();
-
+            int i = 0;
             var response = _fluentEmail!
-                .To("test1@test.com")
+                .To($"test{i}@test.com")
                 .Subject("test suject")
                 .Body("test body")
                 .Send();
+
+            if (!response.Successful)
+                Console.WriteLine(response.MessageId);
+            // }
+            //);
         }
     }
 }
