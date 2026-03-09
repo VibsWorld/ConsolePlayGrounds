70533bfe076320241657d44c33c4fb3451341348
Vaibhav Agarwal <vaibhav@parcelhero.com>
2026-03-09 22:23:32 +0530

    init

---
+++ EventStore/EventStore.SyncApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EventStore.SyncApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
